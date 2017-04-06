aed94e6707e16ac6bc427c0bab103b51e2fb7f89
Dabaitu2 <331555765@qq.com>
2017-04-07 01:23:31 +0800

    More Changes

--- public/js/app.js
+++ public/js/app.js
@@ -19,13 +19,20 @@ function find(ary,match) {
 const rules = [
     {path:'/',controller:controllers.home},
     {path:'/user',controller:controllers.user},
-    {path:/^\/static(\/.*)/,controller:controllers.static},
+    {path:'/auth/register',controller:controllers.auth.register},
+    {path:'/auth/login',controller:controllers.auth.login},
+    {path:/^\/static(\/.*)/,controller:controllers.static}
 ];
 
 var server = http.createServer(function (req,res) {
     var urlInfo = parseUrl(req.url);
-    console.log('url:',req.url,'urlInfo',urlInfo);
+    // console.log('url:',req.url,'urlInfo',urlInfo);
     var rule = find(rules,function(rule){
+        if(rule.method){
+            if(rule.method.toLowerCase()!==req.method.toLowerCase()){
+                return false;
+            }
+        }
         if(rule.path instanceof RegExp){
             var matchResult = urlInfo.pathname.match(rule.path);
             if(matchResult){
